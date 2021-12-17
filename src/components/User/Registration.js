ae0ad1a2cc007aa86b1b528543ef7d7acab07a86
Diganto Bhowmik <digantobhowmik@gmail.com>
2021-12-17 23:28:28 +0600

    validation in registration

--- src/components/User/Registration.js
+++ src/components/User/Registration.js
@@ -9,31 +9,96 @@ const Registration = () => {
         name:"",
         email:"",
         phone:"",
-        password:""
+        password:"",
+        Confirm_Password:""
     })
+
+    const validateEmail = (email) =>
+        email.match(
+          /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    
+
+    const history=useHistory();
+
+    const [nameErr, setNameErr] = useState("");
+    const [emailErr, setEmailErr] = useState("");
+    const [phoneErr, setPhoneErr] = useState("");
+    const [passwordErr, setPasswordErr] = useState("");
+    const [rpasswordErr, setRpasswordErr] = useState("");
+
+      
     const handleChange=(e)=>{
-        const newUser={...user};
-        newUser[e.target.name]= e.target.value
-        setUser(newUser);
+      const newUser={...user};
+      newUser[e.target.name]= e.target.value
+      setUser(newUser);
     }
-    const history=useHistory()
+
     const handleSubmit=(e)=>{
         
         console.log(user)
-        axios.post('http://127.0.0.1:8000/api/register',user)
-        .then(resp=>{
-            if (!resp.data) {
-              console.log('erorr')
-          } else {
-            localStorage.setItem('userId',resp.data.id)
-            localStorage.setItem('userData',resp.data)
-            history.push('/');
-          }
-        }).catch(err=>{
-          console.log(err);
-        });
         e.preventDefault();
+        if(user.name !== ""  && user.phone !== "" && user.email !== "" &&  user.password !== "" && user.Confirm_Password !== "" && user.password===user.Confirm_Password ) 
+        {
+            setNameErr("")
+            setPhoneErr("")
+            setPasswordErr("")
+            setRpasswordErr("")
+            axios.post('http://127.0.0.1:8000/api/register',user)
+            .then(resp=>{
+              if (!resp.data) {
+                console.log('erorr')
+            } else {
+              localStorage.setItem('userId',resp.data.id)
+              localStorage.setItem('userData',resp.data)
+              history.push('/');
+            }
+           }).catch(err=>{
+              console.log(err);
+              setEmailErr("This mail already used")
+            });
+           
+        }
+        else{
+            if(user.name === "")
+                setNameErr("Please fill the name");
+            else
+                setNameErr("")
+
+            if(user.phone === "")
+                setPhoneErr("Please fill the phone number");
+            else
+                setPhoneErr("")
+
+            if(user.email === "")
+                setEmailErr("Please fill the Email");
+            else
+                setEmailErr("")
+
+            if(!validateEmail(user.email))
+                setEmailErr("Please enter valid mail");
+            else
+                setEmailErr("");
+
+            if(user.password === "")
+                setPasswordErr("Please fill the Password");
+            else
+                setPasswordErr("")
+
+            if(user.Confirm_Password === "")
+                setRpasswordErr("Please fill the Confirm Password");
+            else
+                setRpasswordErr("")
+
+            if(user.password !== user.Confirm_Password)
+                setRpasswordErr("Doestn't Match");
+            else
+                setRpasswordErr("")
+        }
+        
     }
+
+    
     return (
       <div>
         <Header/>
@@ -43,28 +108,28 @@ const Registration = () => {
             <div className="mb-3">
                 <label for="formGroupExampleInput" className="form-label">Full Name :</label>
                 <input type="text" name="name" className="form-control" id="formGroupExampleInput" onChange={handleChange}/>
-
+                <p style={{color: "red"}}>{nameErr}</p>
               </div>
             <div className="mb-3">
                 <label for="formGroupExampleInput" className="form-label" style={{marginTop: "20px"}}>Email :</label>
                 <input type="text" name="email" className="form-control" id="formGroupExampleInput" onChange={handleChange}/>
-                
+                <p style={{color: "red"}}>{emailErr}</p>
               </div>
               <div className="mb-3">
                 <label for="formGroupExampleInput" className="form-label" style={{marginTop: "20px"}}>Phone :</label>
                 <input type="text" name="phone" className="form-control" id="formGroupExampleInput" onChange={handleChange}/>
-                
+                <p style={{color: "red"}}>{phoneErr}</p>
               </div>
 
               <div className="mb-3" style={{marginTop: "20px"}}>
                 <label for="formGroupExampleInput2" className="form-label">Password :</label>
                 <input type="password" name="password" className="form-control" id="formGroupExampleInput2" onChange={handleChange}/>
-                
+                <p style={{color: "red"}}>{passwordErr}</p>
               </div>
               <div className="mb-3" style={{marginTop: "20px"}}>
                 <label for="formGroupExampleInput2" className="form-label">Confirm Password :</label>
                 <input type="password" name="Confirm_Password" className="form-control" id="formGroupExampleInput2" onChange={handleChange}/>
-                
+                <p style={{color: "red"}}>{rpasswordErr}</p>
               </div>
               <input type="submit" name="submit" value="REGISTER" className="btn btn-primary login_button"/>
               <div className="swap_between_login_register">
